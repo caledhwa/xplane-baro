ff6e757a56d5376f6a1f2c80d2eb5f09fcdd9767
Cale Hoopes <calehoopes@Teresas-MacBook-Air.local>
2021-10-05 07:34:14 -0700

    Encoder working

--- xplane-baro.ino
+++ xplane-baro.ino
@@ -1,3 +1,5 @@
+#define VERSION 1.20
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -7,12 +9,12 @@
 String ssid = "Home-2001";
 String pskey = "bluedog2014";
 
+#pragma region UDP-WIFI
 WiFiUDP udp;
-
+int retry = 30;
 IPAddress my_IP;
 IPAddress multicastIP (239,255,1,1);  // Do not change this
 IPAddress xplane_ip;
-
 uint8_t beacon_major_version;
 uint8_t beacon_minor_version;
 uint32_t application_host_id;
@@ -22,65 +24,103 @@ uint32_t role;
 uint16_t port;
 char xp_hostname[32];
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1];
+unsigned int my_port = 3017;      // Could be anything, this just happens to be my favorite port number
+unsigned int xplane_port = 49000; // Don't change this
+unsigned int beacon_port = 49707; // or this...
+char buffer[1024];
+#pragma endregion
 
 #define STATE_IDLE 0
 #define STATE_SEARCH 1
 #define STATE_READY 2
 
-char buffer[1024];
+#define BARO_DREF "sim/cockpit/misc/barometer_setting"
+#define BARO_DREF_UID 42
+#define RREF "RREF"
 
 int state = STATE_IDLE;
-unsigned int my_port = 3017;      // Could be anything, this just happens to be my favorite port number
-unsigned int xplane_port = 49000; // Don't change this
-unsigned int beacon_port = 49707; // or this...
-int retry = 30;
+
 int pot = 0;
-float current_baro = 0;
+float display_baro = 29.92;
 float sim_baro = 0;
 
 LedControl lc = LedControl(D7,D5,D4,1);
+
+#define ENC_CLK D1
+#define ENC_DT D2
+#define ENC_SW D3
+
+int counter = 0;
+int currentStateCLK;
+int lastStateCLK;
+String currentDir ="";
+bool isButtonPressed = false;
+unsigned long lastButtonPress = 0;
+unsigned long lastTurn = 0;
+
+void IRAM_ATTR buttonPress() {
+  if (isButtonPressed && millis() - lastButtonPress > 50) {
+    isButtonPressed = false;
+    lastButtonPress = millis();
+  } else {
+    Serial.println("Button pressed");
+    isButtonPressed = true;
+    display_baro = 29.92f;
+    writeFloat(BARO_DREF, display_baro);
+  }
+}
+
+void updateBaro(int inc) {
+  int thousandInt = display_baro * 100;
+  int incrementValue = thousandInt + inc;
+  display_baro = incrementValue / 100.0f;
+  writeFloat(BARO_DREF, display_baro);
+}
+
+void IRAM_ATTR updateEncoder(){
+	currentStateCLK = digitalRead(ENC_CLK);
+	if (currentStateCLK != lastStateCLK  && currentStateCLK == 1 && millis() - lastTurn > 25){
+		if (digitalRead(ENC_DT) != currentStateCLK) {
+      updateBaro(-1);
+			counter --;
+			currentDir ="CCW";
+		} else {
+      updateBaro(+1);
+			counter ++;
+			currentDir ="CW";
+		}
+		Serial.println("Direction: " + currentDir + ", " + String(counter) + ", " + String(display_baro,2));
+    lastTurn = millis();
+	}
+	lastStateCLK = currentStateCLK;
+}
 
 void setup() {
 
   retry = 30;
   Serial.begin(115200);
-  Serial.println("\nX-Plane UDP Interface 0.9\n");
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pskey);
-
-  Serial.println("Attempting Connection to Network: ");
-  Serial.println(ssid);
-
-  // Wait for connection
-  while ((retry-- > 0) && (WiFi.status() != WL_CONNECTED)) {
-    delay(250);
-    Serial.print(".");
-  }
-
-  if (retry > 0) {
-    my_IP = WiFi.localIP();
-    Serial.println("connected.  My IP = ");
-    Serial.println(my_IP);
-  }
-  else {
-    Serial.println("Unable to connect");
-    return;
-  }
-
-  Serial.println("Searching for X-Plane");
-  retry = 30;
-  udp.beginMulticast(my_IP, multicastIP, beacon_port);
-  state = STATE_SEARCH;
+  Serial.println("X-Plane Altimeter Baro Control " + String(VERSION));
+  
+  connect();
 
   pinMode(LED_BUILTIN, OUTPUT); 
-  pinMode(A0, INPUT); 
 
-  Serial.println("Setting up the lc");
   lc.shutdown(0, false);
   lc.setIntensity(0, 15);
   lc.clearDisplay(0);
+
+  state = STATE_SEARCH;
+
+  pinMode(ENC_CLK, INPUT); // CLK
+  pinMode(ENC_DT, INPUT); // DT
+  pinMode(ENC_SW, INPUT_PULLUP); // SW
+
+  lastStateCLK = digitalRead(ENC_CLK);
+  attachInterrupt(ENC_CLK, updateEncoder, CHANGE);
+	attachInterrupt(ENC_DT, updateEncoder, CHANGE);
+  attachInterrupt(ENC_SW, buttonPress, RISING);
 }
+
 
 void loop() {
 
@@ -101,46 +141,33 @@ void loop() {
 }
 
 void ready() {
+  displayDigits(display_baro);
+
   int packetSize = udp.parsePacket();
-  pot = analogRead(A0);  
-  float pot_baro = 31.0f - (float(pot/1023.00f) * 2.00f);
-
-  if (pot_baro != current_baro) {
-    writeFloat("sim/cockpit/misc/barometer_setting", pot_baro);
-    current_baro = pot_baro;
-  }
-
-  int i = sim_baro * 100;
-  Serial.println("Pot Baro: " + String(pot_baro, 2) + ", Sim Baro: " + String(sim_baro,2) + ", I: " + String(i));
-
-  displayDigits(i);
-
   char *buff = &buffer[3];  // For alignment
   udp.read(buff, packetSize);
   String type =  String(buff).substring(0,4);
 
-  if (type == "RREF") {
+  if (type == RREF) {
     for (int offset = 5; offset < packetSize; offset += 8) {
       int code = *((int *) (buff+offset));
       float value = *((float *) (buff+offset+4));
       switch(code) {                                              
-        case 42:
+        case BARO_DREF_UID:
           int x = value * 100;
-          if (x != 4800) {
-            sim_baro = value;
-          }
+          Serial.println("Value: " + String(value) + ", X:" + String(x));
+          display_baro = value;
           break;
       }
     }
   }
-
   delay(100);
 }
 
 void subscribe(char *dref, uint32_t freq, uint32_t index) {
   struct {
     char dummy[3]; // For alignment
-    char hdr[5] = "RREF";
+    char hdr[5] = RREF;
     uint32_t dref_freq;
     uint32_t dref_en;
     char dref_string[400];
@@ -208,13 +235,12 @@ void searchForXPlane() {
     state = STATE_READY;
     udp.begin(my_port);
 
-    subscribe("sim/cockpit/misc/barometer_setting", 3, 42);
+    subscribe(BARO_DREF, 2, BARO_DREF_UID);
   }
 }
 
-
-void displayDigits(int i) {
-  Serial.println("Input int: {" + String(i) + "}");
+void displayDigits(float input) {
+  int i = input * 100;
   int thousandsDigit = (int)i/1000;
   int hundredsDigit = ((int)i/100*100-(int)i/1000*1000)/100;
   int tensDigit = ((int)i/10*10-(int)i/100*100)/10;
@@ -224,4 +250,32 @@ void displayDigits(int i) {
   lc.setDigit(0,2,hundredsDigit, true);
   lc.setDigit(0,1,tensDigit, false);
   lc.setDigit(0,0,onesDigit, false);
+}
+
+void connect() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pskey);
+
+  Serial.println("Attempting Connection to Network: ");
+  Serial.println(ssid);
+
+  // Wait for connection
+  while ((retry-- > 0) && (WiFi.status() != WL_CONNECTED)) {
+    delay(250);
+    Serial.print(".");
+  }
+
+  if (retry > 0) {
+    my_IP = WiFi.localIP();
+    Serial.println("connected.  My IP = ");
+    Serial.println(my_IP);
+  }
+  else {
+    Serial.println("Unable to connect");
+    return;
+  }
+
+  Serial.println("Searching for X-Plane");
+  retry = 30;
+  udp.beginMulticast(my_IP, multicastIP, beacon_port);
 }
